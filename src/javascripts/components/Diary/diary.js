f72992353f8f9df3e872eabd1788fd0770857543
JPantana <joshpantana@me.com>
2019-06-08 09:13:13 -0500

    wip

--- src/javascripts/components/Diary/diary.js
+++ src/javascripts/components/Diary/diary.js
@@ -3,9 +3,40 @@ import diaryData from '../../helpers/data/diaryData';
 // scss
 import './diary.scss';
 
+const newDiaryPost = (e) => {
+  e.preventDefault();
+  const newDiaryPostTitle = document.getElementById('diaryPostTitleInput').value;
+  const newDiaryPostImg = document.getElementById('imageUrlInput').value;
+  const newDiaryPostRating = document.getElementsByClassName('diaryPostRatingCheckBox').checked;
+  const replaceDiaryPostObj = {
+    title: newDiaryPostTitle,
+    imageUrl: newDiaryPostImg,
+    diaryPostRating: newDiaryPostRating,
+    stars: 0,
+  };
+  const radioBtns = document.getElementsByName('rating');
+  radioBtns.forEach((radio) => {
+    if (radio.checked) {
+      replaceDiaryPostObj.diaryPostRating = radio.value;
+    }
+  });
+  diaryData.makeNewDiaryPost(replaceDiaryPostObj)
+    .then(() => {
+      diaryDomStringBuilder() // eslint-disable-line no-use-before-define
+        .then()
+        .catch(err => console.error('no diaryPosts called', err));
+    }).catch(err => console.error(err));
+};
+
+const diaryFormInputBuilder = () => {
+  console.error(Date.now());
+};
+
 const diaryDomStringBuilder = () => {
   let domString = '<div class="col diaryCardsDiv">';
-  domString += '<div><h1 class="h1 text-dark text-center">Diary</h1></div>';
+  domString += `
+  <div><h1 class="h1 text-dark text-center">Diary</h1></div>
+  <div class="btnPosition"><button class="coolBtn">Add Post</button></div>`;
   diaryData.getDiaryPostByUid().then((diaryPosts) => {
     diaryPosts.forEach((post) => {
       domString += `
@@ -20,6 +51,7 @@ const diaryDomStringBuilder = () => {
     domString += '</div>';
     util.printToDom('diaryComponentDiv', domString);
   }).catch(err => console.error('could not get diary post', err));
+  // diaryFormInputBuilder();
 };
 
-export default { diaryDomStringBuilder };
+export default { diaryDomStringBuilder, diaryFormInputBuilder, newDiaryPost };
