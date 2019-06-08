b005e61f98aa857d58da026865d81d912428a8a1
Mary West <westmarye1219@gmail.com>
2019-06-08 10:35:38 -0500

    read complete

--- src/javascripts/components/Messages/messages.js
+++ src/javascripts/components/Messages/messages.js
@@ -2,14 +2,23 @@ import messsagesData from '../../helpers/data/messagesData';
 import util from '../../helpers/util';
 
 const messagesStringBuilder = () => {
+  let domString = '<div class="col messageCardsDiv">';
   messsagesData.getMessagesByUid()
     .then((messages) => {
-      let domString = '';
       messages.forEach((message) => {
-        domString += `<h1>${message.message}</h1>`;
-        domString += `<h6 class= "timeStamp">${message.timeStamp}</h6>`;
-        domString += `<h2 id =${message.id} class="edit-btn">${message.isEdited}</h2>`;
-        domString += `<h2 id =${message.id} class="delete-btn"></h2>`;
+        domString += '<div class="card">';
+        domString += `<h2 id="username">${message.uid}</h2>`;
+        domString += '<div class="input-group">';
+        domString += `<textarea class="form-control editBox  hide" id=${message.id} aria-label="With textarea">${message.messageText}</textarea>`;
+        domString += `<div id=${message.id}><p>${message.message}</p></div>`;
+        domString += '</div>';
+        domString += `<button type="button" id="${message.id}" class="btn btn-danger edit">Edit</button>`;
+        domString += `<button type="button" id="${message.id}" class="btn btn-danger save  hide">Save</button>`;
+        domString += `<p id="id">Message ID: ${message.id} </p>`;
+        domString += `<h6 id="timestamp">${message.timestamp} </h6>`;
+        domString += `<button type="button" id="${message.id}" class="btn btn-danger delete">Delete</button>`;
+        domString += '</div>';
+        domString += '</div>';
       });
       util.printToDom('messagesComponentDiv', domString);
     })
