754de328d3764da32f0f6766908b8646959f7560
Silvestre Luna <slvtrluna@gmail.com>
2019-06-09 01:12:14 -0500

    completed read me ticket and added styling

--- src/javascripts/components/Events/events.js
+++ src/javascripts/components/Events/events.js
@@ -4,19 +4,20 @@ import './events.scss';
 
 const printEventsToDom = (array) => {
   let itemToPrint = '<div class="card events-container">';
-  itemToPrint += '<h1>Events</h1>';
+  itemToPrint += '    <h1>Events</h1>';
+  itemToPrint += '    <img class="add-new-event-Button" src="../../../../assets/newEventIcon.png" />';
   array.forEach((event) => {
-    itemToPrint += `<div id="${event.id}" class="each-event-card " >`;
-    itemToPrint += '<section>';
-    itemToPrint += '<img class="deleteIcon" src="https://image.flaticon.com/icons/png/512/61/61795.png" />';
-    itemToPrint += '</section>';
-    itemToPrint += `<p>${event.eventName} </p>`;
-    itemToPrint += '<div class="showMoreDetails d-none">';
-    itemToPrint += `<p>${event.eventDate}..${event.eventLocation}</p>`;
-    itemToPrint += '</div>';
-    itemToPrint += '</div>';
+    itemToPrint += `  <div id=${event.id} class="action-icons-div">`;
+    itemToPrint += '    <img class="editIcon" src="../../../../assets/editBtn.svg" />';
+    itemToPrint += '    <img class="deleteIcon" src="../../../../assets/deleteBtn.png" />';
+    itemToPrint += '  </div>';
+    itemToPrint += `  <div id="card.${event.id}" class="each-event-card" >`;
+    itemToPrint += `    <p class="event-date"><img class="eventIcon" src="../../../../assets/eventIcon.png" />${event.eventDate}</p>`;
+    itemToPrint += `    <p>${event.eventName} </p>`;
+    itemToPrint += `    <p class="event-location">${event.eventLocation}</p>`;
+    itemToPrint += '  </div>';
   });
-  itemToPrint += '</div>';
+  itemToPrint += '   </div>';
   util.printToDom('eventsComponentDiv', itemToPrint);
 };
 
