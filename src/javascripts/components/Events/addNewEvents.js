ae6cb23071b2ec85faebadafa9766283edceca70
Silvestre Luna <slvtrluna@gmail.com>
2019-06-10 20:00:50 -0500

    wip

--- src/javascripts/components/Events/addNewEvents.js
+++ src/javascripts/components/Events/addNewEvents.js
@@ -1,17 +1,32 @@
 import $ from 'jquery';
 
-// import util from '../../helpers/util';
+import util from '../../helpers/util';
 
 // const newEventForm = () => {
 //   const itemToPrint = '<h1>test</h1>';
 // };
+const diaryFormInputBuilder = () => {
+  const diaryString = '';
+  const domString = `
+  <div>
+    <form id="eventFormCreate" class="form-group">
+    <h1>Add new Event</h1>
+    <label for="EventDateInput">Date</label><input id="EventDateInput" type="date"></input>
+      <label for="eventNameInput">Event Name</label><input id="eventNameInput" type="text"></input>
+      <label for="eventLocationInput">Address</label><input id="eventLocationInput" type="text"></input>
+      <button id="createNewEvent" type="submit" class="btn btn-primary">Create Event</button>
+    </form>
+  </div>`;
+  util.printToDom('addNewDiaryPostFormDiv', diaryString);
+  util.printToDom('addNewCalendarEvent', domString);
+};
 
 const AddNewEvent = (e) => {
   const test = e.target.id;
   console.error(test);
-  $('#newEventModal').modal().show();
+  $('#pineModal').modal().show();
+  diaryFormInputBuilder();
 };
-
 
 const eventListenerForPageLoad = () => {
   const getBtnId = document.getElementsByClassName('add-new-event-Button');
