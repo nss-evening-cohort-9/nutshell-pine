87d8deb87f3cadc667d80bcf5a6d196fa27f6f47
Mary West <westmarye1219@gmail.com>
2019-06-13 20:16:30 -0500

    users work

--- src/javascripts/components/Users/users.js
+++ src/javascripts/components/Users/users.js
@@ -2,40 +2,56 @@
 import 'firebase/auth';
 import 'bootstrap';
 import usersData from '../../helpers/data/usersData';
+import util from '../../helpers/util';
+// jQuery
+import $ from '../../../../node_modules/jquery';
+
+// modal
+const userModal = (users) => {
+  const domString = `
+  <div class="container">
+    <form class="col">
+      <label for="username-input">Create Username</label>
+      <input id="username-input"></input>
+      <label for="defaultForm-email">Add Email</label>
+      <input id="defaultForm-email"></input>
+      <button id="submit-user-info" type="submit" class="btn btn-success">Sign Up!</button>
+    </form>
+  </div>`;
+  util.printToDom('user-modal', domString);
+  $('#pineModal').modal('toggle');
+  document.getElementById('submit-user-info').addEventListener('click', (e) => {
+    e.preventDefault();
+    addNewUser(users); // eslint-disable-line no-use-before-define
+    $('#pineModal').modal('toggle');
+  });
+};
 
 
-const newUserValues = () => {
+// const getCurrentUid = () => firebase.auth().currentUser.uid;
+
+const newUserValues = (users) => {
   const newUser = {
     userName: document.getElementById('username-input').value,
-    message: document.getElementById('defaultForm-email').value,
+    email: document.getElementById('defaultForm-email').value,
+    uid: users,
   };
   return newUser;
 };
 
-const addNewUser = () => {
-  const newUserObject = newUserValues();
+
+const addNewUser = (users) => {
+  const newUserObject = newUserValues(users);
   const userInput = newUserObject.user;
   if (userInput !== '') {
-    usersData.createNewUser(newUserObject)
-      .then(() => {
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    usersData.createNewUser(newUserObject).then().catch(error => console.error(error));
   }
 };
 
-// const enterUserSubmit = () => {
-//   $('#new-user-modal').on('keyup', (e) => {
-//     if (e.keyCode === 13) {
-//       $('#submit-user-info').trigger('click');
-//     }
-//   });
+
+// const events = () => {
+//   document.getElementById('submit-user-info').addEventListener('click', addNewUser);
+//   console.error('click-event');
 // };
 
-const events = () => {
-  document.getElementById('submit-user-info').addEventListener('click', addNewUser);
-  console.error('click-event');
-};
-
-export default { events };
+export default { addNewUser, userModal };
--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -7,6 +7,7 @@ import diary from '../../components/Diary/diary';
 import messages from '../../components/Messages/messages';
 import events from '../../components/Events/events';
 import users from '../../components/Users/users';
+import usersData from './usersData';
 
 const authDiv = document.getElementById('auth');
 const layoutDiv = document.getElementById('layout');
@@ -14,7 +15,7 @@ const nutshellNavbar = document.getElementById('navbar-button-nutshell');
 const authNavbar = document.getElementById('navbar-button-auth');
 const logoutNavbar = document.getElementById('navbar-button-logout');
 const footer = document.getElementById('footer');
-const newUserBtn = document.getElementById('new-user-btn');
+// const newUserBtn = document.getElementById('new-user-btn');
 
 // const getCurrentUserName = () => firebase.auth().currentUser.displayName;
 
@@ -23,9 +24,24 @@ const newUserBtn = document.getElementById('new-user-btn');
 const checkLoginStatus = () => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
+      usersData.getUsers()
+        .then((allUsers) => {
+          // allUsers.forEach((u) => {
+          //   console.error(u.uid);
+          // });
+          const filterUids = allUsers.filter(id => id.uid === user.uid);
+          // console.error(filterUids);
+          // console.error(user.uid);
+          // console.error(allUsers.length);
+          if (filterUids.length === 0) {
+            // console.error(filterUids.length);
+            users.userModal(user.uid);
+          }
+        })
+        .catch(err => console.error('getting single user at authData', err));
       dashboard.dashboardLayoutStringBuilder();
       authDiv.classList.add('hide');
-      newUserBtn.classList.add('hide');
+      // newUserBtn.classList.add('hide');
       layoutDiv.classList.add('show');
       nutshellNavbar.classList.remove('hide');
       authNavbar.classList.add('hide');
@@ -36,16 +52,15 @@ const checkLoginStatus = () => {
       messages.displayMsgInput();
       messages.initMessages();
       events.initEventsForPageLoad(user.uid);
-      // users.getUserName(getCurrentUid);
     } else {
       authDiv.classList.remove('hide');
-      newUserBtn.classList.remove('hide');
+      // newUserBtn.classList.remove('hide');
       layoutDiv.classList.remove('show');
       nutshellNavbar.classList.add('hide');
       authNavbar.classList.remove('hide');
       logoutNavbar.classList.add('hide');
       footer.classList.add('hide');
-      users.events();
+      // users.events(); // testing thing. where the events took place prior to users.js
     }
   });
 };
